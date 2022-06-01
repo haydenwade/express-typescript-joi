1eea8b68c18b4c7ab83b63e1ca2c7c8030bdcfb5
haydenwade <hwade@sundae.com>
2022-06-01 14:43:04 -0500

    Remove extra schema

--- src/schemas/SetMileageSchemas.ts
+++ src/schemas/SetMileageSchemas.ts
@@ -14,29 +14,4 @@ export const ComplexBodySchema = Joi.object({
   b: Joi.number().required(),
   x: Joi.boolean().required(),
 }).meta({className: "ComplexBodySchema"});
-
-
-export const PropertiesSchemaV2 = Joi.object()
-  .keys({
-    page: Joi.number().required().min(0),
-    size: Joi.number().required().min(1),
-    sortBy: Joi.string().optional().max(255),
-    sortOrder: Joi.string().optional().max(255),
-    territory: Joi.string().optional().max(255),
-    propertyStatus: Joi.string().optional().max(255),
-    startDate: Joi.date().timestamp('unix').optional(),
-    dateField: Joi.string().optional().max(255),
-    distance: Joi.object()
-      .keys({
-        include: Joi.boolean().required(),
-        fromLatitude: Joi.number().required().min(-90).max(90),
-        fromLongitude: Joi.number().required().min(-180).max(180)
-      })
-      .optional()
-      .when('sortBy', {
-        is: 'distance',
-        then: Joi.required()
-      })
-  })
-  .and('dateField', 'startDate');
 //#endregion
